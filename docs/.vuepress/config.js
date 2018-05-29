ef691ecff134c2cc17a3d04b8d235907ef3990a0
Daniel Lerch <daniel@lerchpanther.ch>
2018-05-29 11:40:50 +0200

    reduxed tree

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'Rahel Redux',
+    description: 'Reduxed Example for Vuepress and Netlify CMS',
+    themeConfig: {
+        nav: [{
+                text: 'theater',
+                link: '/theater'
+            },
+            {
+                text: 'film',
+                link: '/film'
+            },
+            {
+                text: 'radio',
+                link: '/radio'
+            }
+        ],
+        sidebar: false
+    }
+}
